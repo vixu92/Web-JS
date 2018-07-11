2508b420bc5a989d97335d240d67d5c3fe3c2be5
vixu92 <vixu92@yahoo.es>
2018-07-11 09:49:13 +0200

    Coma

--- Estadistica/fibonacci.js
+++ Estadistica/fibonacci.js
@@ -1,22 +1,21 @@
-function fibonacci(numero){
+function fibonacci(){
 
     var cantidad = prompt("Cuantas cifras de la serie desea mostrar?");
     var var1 = 0;
     var var2 = 1;
     var var3;
 
-cantidad = parseInt(cantidad);
+    cantidad = parseInt(cantidad);
 
-    document.write(var1 + "");
-    document.write(var2 + "");
+    document.write(var1 + ", ");
+    document.write(var2 + ", ");
 
-    for(var i = 3; i <= numero ; i++)
-    {
+    for(var i = 3; i <= cantidad ; i++){
+
         var3 = var1 + var2;
         var1 = var2;
         var2 = var3;
-        document.write(var3 + "");
+        document.write(var3 + ", ");
     }
 }
- 
-document.write(fibonacci(numero));
+document.write(fibonacci());
